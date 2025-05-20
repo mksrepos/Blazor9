842a7957b945046b26c4d8d906225346c451f27d
Manoj Kumar Sharma <manojexamai@gmail.com>
2025-05-20 12:06:38 +0530

    Updates from Day 02

--- Day01/BlazorWebApp1/BlazorWebApp1/Program.cs
+++ Day01/BlazorWebApp1/BlazorWebApp1/Program.cs
@@ -4,7 +4,8 @@ using BlazorWebApp1.Components;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
+builder.Services
+    .AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
 
