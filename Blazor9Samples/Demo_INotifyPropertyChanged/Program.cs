19739dd1359aadcd0622b6f6c48483d8b40abf59
Manoj Kumar Sharma <manojexamai@gmail.com>
2025-05-20 12:07:08 +0530

    Updates from Day 02 - second set.

---
+++ Blazor9Samples/Demo_INotifyPropertyChanged/Program.cs
@@ -0,0 +1,16 @@
+﻿using Demo_INotifyPropertyChanged;
+
+Product product = new Product()
+{
+    Id = 1,
+    Name = "First Product",
+    Quantity = 10
+};
+
+product.Quantity = 50;
+
+product.PropertyChanged += ( sender, e ) =>
+{
+    Console.WriteLine(e.PropertyName + " got changed. ");
+};
+product.Quantity += 10;
